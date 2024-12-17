b3ac3266a40359e61fa22479baa389f8a99028f7
Баухажан Санжар <171016409@step.kz>
2024-12-17 17:16:15 +0500

    with cycle added smartphone in shell

--- new_app/models.py
+++ new_app/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField(blank=False)
+    tags = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.name
