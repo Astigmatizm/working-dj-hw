61895386e00f92f57552c8fdb88583d245f0989e
Баухажан Санжар <171016409@step.kz>
2024-12-20 15:38:15 +0500

    added htmlbody and new urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import IceCreamViewSet, IceCreamKioskViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'icecream', IceCreamViewSet)
@@ -8,4 +9,5 @@ router.register(r'kiosk', IceCreamKioskViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),  # Подключаем маршруты, сгенерированные маршрутизатором
+    path('get_string/', views.get_string, name='get_string'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import IceCream, IceCreamKiosk
 from .serializers import IceCreamSerializer, IceCreamKioskSerializer
+from django.http import HttpResponse
+
+
+def get_string(request):
+    html_content = """
+        <html>
+            <head>
+                <title>String Response</title>
+            </head>
+            <body>
+                <h1>эта строка была возвращена из фронтенда</h1>
+            </body>
+        </html>
+        """
+    return HttpResponse(html_content)
+
 
 class IceCreamViewSet(viewsets.ModelViewSet):
     queryset = IceCream.objects.all()
     serializer_class = IceCreamSerializer
+
 
 class IceCreamKioskViewSet(viewsets.ModelViewSet):
     queryset = IceCreamKiosk.objects.all()
