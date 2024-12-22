9e308c82178d7e26d4ff509690a5520b1d62a52e
Баухажан Санжар <171016409@step.kz>
2024-12-22 17:43:54 +0500

    deffect

--- SiteApp/urls.py
+++ SiteApp/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # path('register/', register, name='register'),
+    path('', index, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
--- SiteApp/views.py
+++ SiteApp/views.py
@@ -6,33 +6,36 @@ from .forms import RegistrationForm
 from django.contrib import messages
 
 
-def show_request_params(request):
-    get_params = request.GET
-    post_params = request.POST
+class index()
 
-    get_params_str = ', '.join([f"{key}: {value}" for key, value in get_params.items()]) # метод items() позволяет получить все ключи и значения в параметрах запроса.
 
-    post_params_str = ', '.join([f"{key}: {value}" for key, value in post_params.items()])
-
-    response_content = f"""
-    <html>
-        <body>
-            <h2>GET Parameters:</h2>
-            <p>{get_params_str if get_params else "Нету GET параметр"}</p>
-            <h2>POST Parameters:</h2>
-            <p>{post_params_str if post_params else "Нету POST параметр"}</p>
-        </body>
-    </html>
-    """
-
-    return HttpResponse(response_content)
+# def show_request_params(request):
+#     get_params = request.GET
+#     post_params = request.POST
+#
+#     get_params_str = ', '.join([f"{key}: {value}" for key, value in get_params.items()]) # метод items() позволяет получить все ключи и значения в параметрах запроса.
+#
+#     post_params_str = ', '.join([f"{key}: {value}" for key, value in post_params.items()])
+#
+#     response_content = f"""
+#     <html>
+#         <body>
+#             <h2>GET Parameters:</h2>
+#             <p>{get_params_str if get_params else "Нету GET параметр"}</p>
+#             <h2>POST Parameters:</h2>
+#             <p>{post_params_str if post_params else "Нету POST параметр"}</p>
+#         </body>
+#     </html>
+#     """
+#
+#     return HttpResponse(response_content)
 
 
 def is_login(request):
     if not request.user.is_authenticated:
         return redirect('login')
 
-    return render(request, 'login.html')
+    return render(request, 'SiteApp/login.html')
 
 
 def login_view(request):
