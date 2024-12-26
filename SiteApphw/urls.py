69cdabc0debb0f449b3c617743997a76d7f685a7
Баухажан Санжар <171016409@step.kz>
2024-12-26 16:48:42 +0500

    failed users

--- SiteApphw/urls.py
+++ SiteApphw/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('send-password-reset-emails/', views.send_reset_password_emails, name='send_reset_password_emails'),
+    path('users/', views.UserListView.as_view(), name='user-list'),  # Для списка всех пользователей
+    path('users/detail/', views.UserDetailView.as_view(), name='user-detail'),  # Для детального просмотра
 ]
--- SiteApphw/views.py
+++ SiteApphw/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from .utils import send_password_reset_email
 from SiteApphw.models import CustomUser  # Импортируем кастомную модель CustomUser
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from django.contrib.auth.models import User
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+
 
 def send_reset_password_emails(request):
     # Получаем всех пользователей, которым нужно отправить письмо
@@ -12,3 +18,21 @@ def send_reset_password_emails(request):
         send_password_reset_email(user)
 
     return HttpResponse("Письма с восстановлением пароля отправлены.")
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'main/user_list.html'  # Шаблон для отображения
+    context_object_name = 'users'  # Имя переменной для передачи в шаблон
+
+
+class UserDetailView(TemplateView):
+    template_name = 'main/user_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.request.GET.get('username')  # Получение параметра из формы
+        if username:
+            user = get_object_or_404(User, username=username)
+            context['user'] = user
+        return context
